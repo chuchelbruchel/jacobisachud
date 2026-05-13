bd991b34df56020615d814df2c14fe0db0647675
chuchelbruchel <rockabyeofficial@gmail.com>
2026-05-13 16:58:36 -0700

    Initialize FastAPI application with routes

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
+from fastapi import FastAPI
+
+# Vercel searches for this 'app' variable
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"status": "success", "message": "FastAPI on Vercel"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int):
+    return {"item_id": item_id}
 
